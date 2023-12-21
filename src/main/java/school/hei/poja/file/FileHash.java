993ee13b61e460be8c8e7441c2ded5c93fb86112
tanjonaaa <hei.tanjona@gmail.com>
2023-12-21 10:19:24 +0300

    fix: deleted useless files

--- src/main/java/school/hei/poja/file/FileHash.java
+++
@@ -1,6 +0,0 @@
-package school.hei.poja.file;
-
-import school.hei.poja.PojaGenerated;
-
-@PojaGenerated
-public record FileHash(FileHashAlgorithm algorithm, String value) {}
--- src/main/java/school/hei/poja/file/FileHashAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package school.hei.poja.file;
-
-import school.hei.poja.PojaGenerated;
-
-@PojaGenerated
-public enum FileHashAlgorithm {
-  SHA256,
-  NONE
-}
